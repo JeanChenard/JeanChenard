7e3706170d72129a7e1b1bffbbec38cf02429c86
JeanChenard <jean.a.chenard@gmail.com>
2021-04-16 16:32:41 -0400

    final(beta)

---
+++ lib2024/final/js/scripts.js
@@ -0,0 +1,22 @@
+// SEQUENCE OF JS
+// 1) User Interacts 
+// 2) Event happens 
+// 3) Function is Called
+// 4) Script runs 
+
+// document.querySelector('#color-1').style.backgroundColor="green";
+
+// window is the keyword targeting current browser
+// window.onload asks the browser to wait for the page to load and then run the function(called init in this case)
+window.onload = init;
+// this is done to avoid complications of function running without having the elements ready and rendered
+
+// function name-of-function will create a function in js file
+// function must have a opening curly brace and a closing one {} similar to css
+// the name of a function is followed by brackets ()
+
+// function createBox() {
+
+// }
+
+function init() {}
